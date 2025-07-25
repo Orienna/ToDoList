9171b8ce6260cd6cb487125b5bc917724ae3bc15
orienna <orichan330@gmail.com>
2025-07-25 13:41:45 -0700

    after iteration 0

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -6,16 +6,42 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @State private var showNewTask = false
+    @Query var toDos: [ToDoItem]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+            HStack {
+                Text("To Do List")
+                     .font(.system(size: 40))
+                     .fontWeight(.black)
+                Spacer()
+                Button {
+                    withAnimation {
+                        showNewTask = true
+                    }
+                    
+                } label: {
+                    Text("+")
+                        .font(.title)
+                        .fontWeight(.bold)
+                }
+            }//HStack
+            .padding()
+            Spacer()
+            List {
+                ForEach (toDos) { toDoItem in
+                    Text(toDoItem.title)
+                    }
+                }
+        }//VStack
         .padding()
+        if showNewTask {
+            NewToDoView()
+        }
     }
 }
 
